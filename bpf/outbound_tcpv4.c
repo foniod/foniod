56b923b77235dbdf1d5c6e0ac985bd7d10920979
Peter Parkanyi <peter@redsift.io>
2018-06-13 16:48:26 +0100

    Kernel space NEEDS to be GPLv2

--- bpf/outbound_tcpv4.c
+++ bpf/outbound_tcpv4.c
@@ -1,3 +1,21 @@
+/*
+ *  Copyright (C) 2018 Authors of RedSift
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 #include "outbound_tcpv4.h"
 
 #pragma clang diagnostic push
@@ -16,7 +34,7 @@ BPF_PERF_OUTPUT(events);
 // Version number to stay compatible with gobpf-elf-loader
 // This should be resolved to running kernel version
 __u32 _version SEC("version") = 0xFFFFFFFE;
-char _license[] SEC("license") = "Apache2";
+char _license[] SEC("license") = "GPL";
 
 SEC("kprobe/tcp_v4_connect")
 int trace_outbound_entry(struct pt_regs *ctx, struct sock *sk)
