f214a5fb2d9c574d7727385c1555d3548d128335
Peter Parkanyi <peter@redsift.io>
2018-06-19 10:16:37 +0100

    Set up perf_event monitors

--- redbpf/src/perf.rs
+++ redbpf/src/perf.rs
@@ -5,7 +5,7 @@
 use bpf_sys::perf_reader::*;
 use bpf_sys::{bpf_open_perf_buffer, bpf_update_elem};
 
-use cpus::{get_online, CpuId};
+use cpus::{self, CpuId};
 use {LoadError, Map, Result, VoidPtr};
 
 use std::ffi::{CStr, CString, NulError};
@@ -22,9 +22,14 @@ unsafe extern "C" fn raw_callback(pc: VoidPtr, ptr: VoidPtr, size: i32) {
 pub type PerfCallback = Box<FnMut(&[u8]) + Send>;
 struct PerfCallbackWrapper(PerfCallback);
 
-pub struct PerfReader(pub RawFd, *mut perf_reader, Box<PerfCallbackWrapper>);
+pub struct PerfReader(*mut perf_reader);
 impl PerfReader {
-    fn new(pid: i32, cpu: i32, page_cnt: i32, callback: PerfCallback) -> Result<PerfReader> {
+    fn new(
+        pid: i32,
+        cpu: i32,
+        page_cnt: i32,
+        callback: PerfCallback,
+    ) -> Result<(PerfReader, RawFd, Box<PerfCallbackWrapper>)> {
         let mut wrapped_cb = Box::new(PerfCallbackWrapper(callback));
         let reader = unsafe {
             ::bpf_sys::bpf_open_perf_buffer(
@@ -42,7 +47,7 @@ impl PerfReader {
         }
 
         let fd = unsafe { perf_reader_fd(reader) };
-        Ok(PerfReader(fd, reader, wrapped_cb))
+        Ok((PerfReader(reader), fd, wrapped_cb))
     }
 }
 
@@ -54,19 +59,61 @@ impl Drop for PerfReader {
 
 pub struct PerfMap<'m> {
     map: &'m Map,
-    page_count: u32,
-    readers: Vec<(CpuId, PerfReader)>,
+    page_count: i32,
+    callbacks: Vec<Box<PerfCallbackWrapper>>,
+    keys: Vec<CpuId>,
+    readers: Vec<PerfReader>,
 }
 
 impl<'m> PerfMap<'m> {
-    fn new(map: &'m Map, page_count: u32) -> Result<PerfMap<'m>> {
+    fn new<CB>(
+        map: &'m mut Map,
+        pid: i32,
+        cpu: i32,
+        page_count: i32,
+        callback: CB,
+    ) -> Result<PerfMap<'m>>
+    where
+        CB: Fn() -> PerfCallback,
+    {
+        let mut readers = vec![];
+        let mut keys = vec![];
+        let mut callbacks = vec![];
+
+        // TODO: Abstract this out to a keying strategy, so keys can be used on
+        // a per-pid/per-port/arbitrary basis
+        for mut cpu in cpus::get_online()? {
+            let (mut r, mut fd, c) = PerfReader::new(pid, cpu, page_count, callback())?;
+
+            map.set(
+                &mut cpu as *mut i32 as VoidPtr,
+                &mut fd as *mut i32 as VoidPtr,
+            );
+
+            // Book-keeping. Same indexes will refer to the relevant object.
+            // The primary reason of not using tuples here is that we can poll()
+            // on readers as a **perf_reader, as seen below.
+            readers.push(r);
+            keys.push(cpu);
+            callbacks.push(c);
+        }
 
         Ok(PerfMap {
             map,
             page_count,
-            readers: vec![],
+            readers,
+            keys,
+            callbacks,
         })
     }
 
-    fn poll(&mut self) {}
+    fn poll(&mut self, timeout: i32) {
+        unsafe {
+            perf_reader_poll(
+                self.readers.len() as i32,
+                self.readers.as_ptr() as *mut *mut perf_reader,
+                timeout,
+            );
+        }
+    }
 }
