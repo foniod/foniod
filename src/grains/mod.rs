5928ba032b2238af6f8a2099c19e70bb90c58c97
Peter Parkanyi <peter@redsift.io>
2018-06-19 18:52:46 +0100

    Grains are falling into place

--- src/grains/mod.rs
+++ src/grains/mod.rs
@@ -1,6 +1,5 @@
-#[allow(non_camel_case_types)]
-mod outbound_tcpv4 {
-    const OUTBOUND_TCPV4: &'static [u8] =
-        include_bytes!(concat!(env!("OUT_DIR"), "/outbound_tcpv4.elf"));
-    include!(concat!(env!("OUT_DIR"), "/outbound_tcpv4.rs"));
+pub mod outbound_tcpv4;
+
+pub trait Grain {
+    fn start();
 }
