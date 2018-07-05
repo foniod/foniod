d276467f354193ec6a0541daef3037a7007dacf9
Peter Parkanyi <peter@redsift.io>
2018-07-05 13:14:19 +0100

    Clean up tags coming from the kernel, use task id instead of pid

--- bpf/connection.h
+++ bpf/connection.h
@@ -25,8 +25,14 @@
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wgnu-variable-sized-type-not-at-end"
 #pragma clang diagnostic ignored "-Waddress-of-packed-member"
+#include <net/inet_sock.h>
+#include <net/sock.h>
 #include <linux/ptrace.h>
 #pragma clang diagnostic pop
+
+#include <linux/version.h>
+#include <linux/bpf.h>
+#include "include/bpf_helpers.h"
 
 struct _data_connect {
   u64 id;
@@ -43,5 +49,23 @@ struct _data_volume {
   size_t send;
   size_t recv;
 };
+
+static __inline__
+struct _data_connect get_connection_details(struct sock **skpp, u64 pid) {
+  struct _data_connect data = {};
+  struct inet_sock *skp = inet_sk(*skpp);
+
+  data.id = pid;
+  data.ts = bpf_ktime_get_ns();
+
+  bpf_get_current_comm(&data.comm, sizeof(data.comm));
+
+  bpf_probe_read(&data.saddr, sizeof(u32), &skp->inet_saddr);
+  bpf_probe_read(&data.daddr, sizeof(u32), &skp->inet_daddr);
+  bpf_probe_read(&data.dport, sizeof(u32), &skp->inet_dport);
+  bpf_probe_read(&data.sport, sizeof(u32), &skp->inet_sport);
+
+  return data;
+}
 
 #endif
