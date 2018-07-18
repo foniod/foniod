bbf5dbacee1ba2086ce0174c7f0aaa3b8b152256
Peter Parkanyi <peter@redsift.io>
2018-07-18 12:33:56 +0100

    Include type information in the name

--- src/backends/s3.rs
+++ src/backends/s3.rs
@@ -4,8 +4,10 @@ pub use rusoto_core::region::Region;
 use rusoto_s3::{PutObjectRequest, S3 as RusotoS3, S3Client};
 use serde_json;
 
+use serde::Serialize;
+
 use backends::Message;
-use metrics::timestamp_now;
+use metrics::{timestamp_now, Measurement, Unit, Tags, kind::Kind};
 
 pub struct S3 {
     hostname: String,
@@ -29,6 +31,32 @@ impl Actor for S3 {
     type Context = Context<Self>;
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct SerializedMeasurement {
+    timestamp: u64,
+    pub kind: Kind,
+    pub name: String,
+    pub measurement: u64,
+    pub tags: Tags,
+}
+
+fn format_by_type(msg: &Measurement) -> impl Serialize {
+    let (type_str, measurement) = match msg.value {
+        Unit::Byte(x) => ("byte", x),
+        Unit::Count(x) => ("count", x),
+    };
+
+    let name = format!("{}_{}", &msg.name, type_str);
+
+    SerializedMeasurement {
+        timestamp: msg.timestamp,
+        kind: msg.kind,
+        name,
+        measurement,
+        tags: msg.tags.clone(),
+    }
+}
+
 impl Handler<Message> for S3 {
     type Result = ();
 
@@ -37,7 +65,7 @@ impl Handler<Message> for S3 {
             Message::List(lst) => format!(
                 "[{}]",
                 lst.iter()
-                    .map(|e| serde_json::to_string(e).unwrap())
+                    .map(|e| serde_json::to_string(&format_by_type(e)).unwrap())
                     .collect::<Vec<String>>()
                     .join(",\n")
             ),
