97d6b5214c9089d641e7228069ae2a7bf8a98dab
Peter Parkanyi <peter@redsift.io>
2018-08-15 17:35:52 +0100

    Remove redundant dockerfiles, create separate one starting from scratch

--- docker/build.sh
+++ docker/build.sh
@@ -5,5 +5,12 @@ if [ "x${cwd##*/}" = "xdocker" ]; then
     cd ..
 fi
 
-docker build -f docker/Dockerfile.builder -t ingraind-build docker
-docker build $@ -f docker/Dockerfile -t ingraind .
+docker build -f docker/Dockerfile.builder \
+       -t quay.io/redsift/ingraind-build:latest \
+       -t ingraind-build \
+       docker
+
+docker build $@ --build-arg KERNEL_SOURCE=/build/kernel/ \
+       -f docker/Dockerfile \
+       -t ingraind \
+       .
