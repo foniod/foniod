4d90b8f919b69351341dfdd5689ffcaa2d3b0c40
Peter Parkanyi <peter@redsift.io>
2018-09-13 16:13:32 +0100

    Move serialization to the core backend module

--- src/backends/mod.rs
+++ src/backends/mod.rs
@@ -1,14 +1,26 @@
 use actix;
+use serde_json;
+
+use std::collections::HashMap;
 
 pub mod console;
 #[cfg(feature = "s3-backend")]
 pub mod s3;
 #[cfg(feature = "statsd-backend")]
 pub mod statsd;
+#[cfg(feature = "http-backend")]
+pub mod http;
 
-use metrics::Measurement;
+use metrics::{Measurement, Unit, kind::Kind};
 
 pub type BackendHandler = actix::Recipient<Message>;
+
+#[derive(Message)]
+pub struct Flush;
+
+impl actix::Message for Measurement {
+    type Result = ();
+}
 
 #[derive(Debug, Clone, Message)]
 pub enum Message {
@@ -16,9 +28,45 @@ pub enum Message {
     List(Vec<Measurement>),
 }
 
-#[derive(Message)]
-pub struct Flush;
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SerializedMeasurement {
+    timestamp: u64,
+    pub kind: Kind,
+    pub name: String,
+    pub measurement: u64,
+    pub tags: HashMap<String, String>,
+}
 
-impl actix::Message for Measurement {
-    type Result = ();
+impl Message {
+    fn to_string(mut self) -> String {
+        match self {
+            Message::List(ref mut lst) => format!(
+                "[{}]",
+                lst.drain(..)
+                    .map(|msg| serde_json::to_string(&SerializedMeasurement::from(msg)).unwrap())
+                    .collect::<Vec<String>>()
+                    .join(",\n")
+            ),
+            Message::Single(msg) => serde_json::to_string(&[SerializedMeasurement::from(msg)]).unwrap(),
+        }
+    }
+}
+
+impl From<Measurement> for SerializedMeasurement {
+    fn from(mut msg: Measurement) -> SerializedMeasurement {
+        let (type_str, measurement) = match msg.value {
+            Unit::Byte(x) => ("byte", x),
+            Unit::Count(x) => ("count", x),
+        };
+
+        let name = format!("{}_{}", &msg.name, type_str);
+
+        SerializedMeasurement {
+            timestamp: msg.timestamp,
+            kind: msg.kind,
+            name,
+            measurement,
+            tags: msg.tags.drain(..).collect(),
+        }
+    }
 }
--- src/backends/s3.rs
+++ src/backends/s3.rs
@@ -1,16 +1,12 @@
-use std::collections::HashMap;
 use std::env;
 
 use actix::prelude::*;
 use futures::Future;
 pub use rusoto_core::region::Region;
 use rusoto_s3::{PutObjectRequest, S3Client, S3 as RusotoS3};
-use serde_json;
 
-use serde::Serialize;
-
-use backends::Message;
-use metrics::{kind::Kind, timestamp_now, Measurement, Unit};
+use backends::{Message};
+use metrics::timestamp_now;
 
 pub struct S3 {
     hostname: String,
@@ -37,47 +33,11 @@ impl Actor for S3 {
     type Context = Context<Self>;
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct SerializedMeasurement {
-    timestamp: u64,
-    pub kind: Kind,
-    pub name: String,
-    pub measurement: u64,
-    pub tags: HashMap<String, String>,
-}
-
-fn format_by_type(mut msg: Measurement) -> impl Serialize {
-    let (type_str, measurement) = match msg.value {
-        Unit::Byte(x) => ("byte", x),
-        Unit::Count(x) => ("count", x),
-    };
-
-    let name = format!("{}_{}", &msg.name, type_str);
-
-    SerializedMeasurement {
-        timestamp: msg.timestamp,
-        kind: msg.kind,
-        name,
-        measurement,
-        tags: msg.tags.drain(..).collect(),
-    }
-}
-
 impl Handler<Message> for S3 {
     type Result = ();
 
-    fn handle(&mut self, mut msg: Message, _ctx: &mut Context<Self>) -> Self::Result {
-        let body = match msg {
-            Message::List(ref mut lst) => format!(
-                "[{}]",
-                lst.drain(..)
-                    .map(|e| serde_json::to_string(&format_by_type(e)).unwrap())
-                    .collect::<Vec<String>>()
-                    .join(",\n")
-            ),
-            Message::Single(msg) => serde_json::to_string(&[format_by_type(msg)]).unwrap(),
-        }.into_bytes()
-        .into();
+    fn handle(&mut self, msg: Message, _ctx: &mut Context<Self>) -> Self::Result {
+        let body = msg.to_string().into_bytes().into();
 
         ::actix::spawn(
             self.client
