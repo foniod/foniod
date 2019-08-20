54e54878eac239831d45e6d3532378a981dc594a
Peter Parkanyi <peter@redsift.io>
2019-08-20 17:58:54 +0100

    Make environment tests parallelizable

--- terraform/run-test.sh
+++ terraform/run-test.sh
@@ -8,13 +8,15 @@ export TF_VAR_ec2_ssh_key_name="$AWS_EC2_SSH_KEY_ID"
 export TF_VAR_ec2_ssh_private_key="$(echo $AWS_EC2_SSH_KEY |tr '|' '\n')"
 export TF_VAR_ec2_os_ami="$OS_AMI"
 
+id=$(dd if=/dev/urandom bs=256 count=1  2>/dev/null|sha1sum |cut -d\  -f1)
+sed "s/RANDOM/$id/" <env.tf.in >env.tf 
+
 terraform init -input=false
 terraform apply -input=false -auto-approve |tee test-output
 
 modules_loaded=$(<test-output awk -F': ' '/ingraind::grains::ebpf: Loaded/ { print $NF }' | sort)
 
 terraform destroy -input=false -auto-approve
-
 
 exec test "$modules_loaded" = "dns_queries, XDP
 tcp_recvmsg, Kprobe
