b4f25f909b713af0b39219d630710f23bd6010a2
Alessandro Decina <alessandro.d@gmail.com>
2019-12-10 10:15:07 +0000

    probes: dns: rustfmt

--- ingraind-probes/src/dns/main.rs
+++ ingraind-probes/src/dns/main.rs
@@ -20,20 +20,20 @@ static mut events: PerfMap<Event> = PerfMap::with_max_entries(1024);
 pub extern "C" fn probe(ctx: XdpContext) -> XdpAction {
     let (ip, transport) = match (ctx.ip(), ctx.transport()) {
         (Some(i), Some(t)) => (unsafe { *i }, t),
-        _ => return XdpAction::Pass
+        _ => return XdpAction::Pass,
     };
     let data = match ctx.data() {
         Some(data) => data,
-        None => return XdpAction::Pass
+        None => return XdpAction::Pass,
     };
 
     let header = match data.slice(12) {
         Some(s) => s,
-        None => return XdpAction::Pass
+        None => return XdpAction::Pass,
     };
 
     if header[2] >> 3 & 0xF != 0u8 {
-        return XdpAction::Pass
+        return XdpAction::Pass;
     }
 
     let event = Event {
