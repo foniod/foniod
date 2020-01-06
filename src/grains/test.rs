62d0287ad2dfab82d861247982b266bdcc40bef9
Alessandro Decina <alessandro.d@gmail.com>
2020-01-06 10:09:52 +0000

    grains: test: add message_len option
    Add message_len option to control how many metrics per message to
    generate. The default if not specified is 5000.

--- src/grains/test.rs
+++ src/grains/test.rs
@@ -1,5 +1,6 @@
 use actix::{Actor, AsyncContext, Context, Recipient, StreamHandler};
 use futures::{Async, Poll, Stream};
+use std::{time, thread};
 
 use crate::backends::Message;
 use crate::metrics::{kind, timestamp_now, Measurement, Tags, Unit};
@@ -19,7 +20,7 @@ impl Actor for TestProbe {
     type Context = Context<Self>;
 
     fn started(&mut self, ctx: &mut Self::Context) {
-        info!("debug probe started");
+        info!("debug probe started {:?}", std::thread::current().id());
         let unit = Unit::try_from_str(
             &self.config.measurement_type,
             self.config.measurement.parse().unwrap_or_default(),
@@ -31,6 +32,7 @@ impl Actor for TestProbe {
             unit,
             kind,
             self.config.tags.clone(),
+            self.config.message_len
         ));
     }
 
@@ -52,15 +54,17 @@ pub struct MeasurementStream {
     unit: Unit,
     kind: kind::Kind,
     tags: Vec<Tag>,
+    message_len: usize
 }
 
 impl MeasurementStream {
-    fn new(name: String, unit: Unit, kind: kind::Kind, tags: Vec<Tag>) -> Self {
+    fn new(name: String, unit: Unit, kind: kind::Kind, tags: Vec<Tag>, message_len: usize) -> Self {
         MeasurementStream {
             name,
             unit,
             kind,
             tags,
+            message_len
         }
     }
 }
@@ -70,26 +74,32 @@ impl Stream for MeasurementStream {
     type Error = ();
 
     fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
-        let mut tags = Tags::new();
-        for (key, value) in &self.tags {
-            let value = match value.as_str() {
-                "$TS" => format!("{}", timestamp_now()),
-                _ => value.clone(),
-            };
-            tags.insert(key.clone(), value);
-        }
-        let measurement = Measurement::new(
-            self.kind,
-            self.name.clone(),
-            self.unit.clone(),
-            tags,
-        );
-        let message = Message::Single(measurement);
+        let measurements = (0..self.message_len).map(|_| {
+            let mut tags = Tags::new();
+            for (key, value) in &self.tags {
+                let value = match value.as_str() {
+                    "$TS" => format!("{}", timestamp_now()),
+                    _ => value.clone(),
+                };
+                tags.insert(key.clone(), value);
+            }
+            Measurement::new(
+                self.kind,
+                self.name.clone(),
+                self.unit.clone(),
+                tags,
+            )
+        }).collect();
+        let message = Message::List(measurements);
         Ok(Async::Ready(Some(message)))
     }
 }
 
 pub type Tag = (String, String);
+
+fn default_message_len() -> usize {
+    5000
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct TestProbeConfig {
@@ -98,4 +108,6 @@ pub struct TestProbeConfig {
     measurement_type: String,
     aggregation_type: Option<String>,
     tags: Vec<Tag>,
+    #[serde(default = "default_message_len")]
+    message_len: usize
 }
