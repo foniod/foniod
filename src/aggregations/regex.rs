a509f3c6b773336930cf515130ec5b02800b970a
Alessandro Decina <alessandro.d@gmail.com>
2020-01-10 14:24:59 +0000

    aggregations: regex: use rayon to parallelize work

--- src/aggregations/regex.rs
+++ src/aggregations/regex.rs
@@ -1,13 +1,15 @@
 use std::collections::HashMap;
+use std::sync::Arc;
 
-use ::actix::prelude::*;
-use futures::Future;
+use actix::prelude::*;
+use rayon::prelude::*;
 use regex::Regex as RegexMatcher;
 
 use crate::backends::Message;
 use crate::metrics::Measurement;
 
-pub struct Regex(HashMap<String, (RegexMatcher, String)>, Recipient<Message>);
+type Rules = Arc<HashMap<String, (RegexMatcher, String)>>;
+pub struct Regex(Rules, Recipient<Message>);
 #[derive(Serialize, Deserialize, Debug)]
 pub struct RegexPattern {
     pub regex: String,
@@ -30,19 +32,10 @@ impl Regex {
                     p.key,
                     (RegexMatcher::new(&p.regex).unwrap(), p.replace_with),
                 )
-            }).collect();
+            })
+            .collect();
 
-        Regex(rules, upstream).start().recipient()
-    }
-
-    fn filter_tags(&self, msg: &mut Measurement) {
-        for (key, value) in msg.tags.iter_mut() {
-            if let Some((regex, replace)) = self.0.get(key) {
-                if regex.is_match(value) {
-                    *value = replace.clone();
-                }
-            }
-        }
+        Regex(Arc::new(rules), upstream).start().recipient()
     }
 }
 
@@ -50,17 +43,28 @@ impl Actor for Regex {
     type Context = Context<Self>;
 }
 
+fn filter_tags(msg: &mut Measurement, rules: Rules) {
+    for (key, value) in msg.tags.iter_mut() {
+        if let Some((regex, replace)) = rules.get(key) {
+            if regex.is_match(value) {
+                *value = replace.clone();
+            }
+        }
+    }
+}
+
 impl Handler<Message> for Regex {
     type Result = ();
 
     fn handle(&mut self, mut msg: Message, _ctx: &mut Context<Self>) -> Self::Result {
+        let rules = self.0.clone();
         match msg {
-            Message::List(ref mut ms) => for mut m in ms {
-                self.filter_tags(&mut m);
-            },
-            Message::Single(ref mut m) => self.filter_tags(m),
+            Message::List(ref mut ms) => ms
+                .par_iter_mut()
+                .for_each(move |m| filter_tags(m, rules.clone())),
+            Message::Single(ref mut m) => filter_tags(m, rules),
         }
 
-        ::actix::spawn(self.1.send(msg).map_err(|_| ()));
+        self.1.do_send(msg).unwrap();
     }
 }
