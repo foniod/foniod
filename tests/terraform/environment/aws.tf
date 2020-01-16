467bcf0dfa3604f3341494455272552db194d06a
Peter Parkanyi <peter@redsift.io>
2020-01-16 16:07:02 +0000

    Add ARM64 test machine on AWS. Just an echo for now

--- tests/terraform/environment/aws.tf
+++ tests/terraform/environment/aws.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-west-2"
+  region = "eu-west-1"
 }
 
 resource "aws_vpc" "ingraind" {
--- tests/terraform/run-amd64.sh
+++ tests/terraform/run-amd64.sh
@@ -1,18 +1,10 @@
 #!/bin/sh
 
-OS_AMI=$1
-
+. ./env.sh
 cp /tmp/workspace/bin/ingraind-$OS_AMI ./ingraind
 
-export TF_VAR_ec2_ssh_key_name="$AWS_EC2_SSH_KEY_ID"
-export TF_VAR_ec2_ssh_private_key="$(echo $AWS_EC2_SSH_KEY |tr '|' '\n')"
-export TF_VAR_ec2_os_ami="$OS_AMI"
-
-id=$(dd if=/dev/urandom bs=256 count=1  2>/dev/null|sha1sum |cut -d\  -f1)
-sed "s/RANDOM/$id/" <env.tf.in >env.tf 
-
 terraform init -input=false
-terraform apply -input=false -auto-approve |tee test-output
+terraform apply -target=null_resource.provision -input=false -auto-approve |tee test-output
 
 modules_loaded=$(<test-output awk -F': ' '/ingraind::grains::ebpf: Loaded/ { print $NF }' | sort)
 
