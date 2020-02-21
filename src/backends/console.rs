02623f1b14b63cc6b3ec36764c46c70256093c20
Peter Parkanyi <peter@redsift.io>
2020-02-21 14:03:17 +0000

    Console now prints JSON-formatted events

--- src/backends/console.rs
+++ src/backends/console.rs
@@ -1,5 +1,6 @@
 use ::actix::prelude::*;
 use crate::backends::Message;
+use crate::backends::encoders::to_json;
 
 #[derive(Default)]
 pub struct Console;
@@ -12,6 +13,13 @@ impl Handler<Message> for Console {
     type Result = ();
 
     fn handle(&mut self, msg: Message, _ctx: &mut Context<Self>) -> Self::Result {
-        println!("{:?}", msg);
+        let mut measurements = match msg {
+            Message::Single(m) => vec![m],
+            Message::List(ms) => ms,
+        };
+
+	for m in measurements.drain(..) {
+            println!("{}", String::from_utf8(to_json(&[m])).unwrap());
+	}
     }
 }
